989545332a6c5de9cb593da348c0b655f5d88c43
Shubham-2122 <shubhamjadav.tops@gmail.com>
2024-12-21 14:56:14 +0530

    react taildwind

--- React/taildwindpro/src/App.jsx
+++ React/taildwindpro/src/App.jsx
@@ -4,7 +4,12 @@ function App() {
 
   return (
     <>
-      <h1 className='text-black bg-pink-400 text-5xl'>hello vite app</h1>
+      <h1 className='text-black italic bg-pink-400 text-5xl mx-10 my-10 px-10 py-20'>hello vite app</h1>
+      <h2 className='text-center text-green-600 mt-5 ml-6 mr-7 mb-10 p-[100px]  bg-slate-400 text-6xl font-mono '>hello this text propet</h2>
+      <h2 className='line-through uppercase m-36 text-5xl decoration-red-800'>asdkjhasikd asds as</h2>
+
+      {/* [] */}
+      <div className='w-1/4 h-72 hover:bg-red-500 hover:text-[rgb(178,233,89)]  bg-[rgba(0,0,0,0.5)] text-[50px] text-[rgb(255,255,255)]'> Hello this</div>
     </>
   )
 }
