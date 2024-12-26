4e8ea0c747c9a597399fcd1f19d54b89899ce736
Shubham-2122 <shubhamjadav.tops@gmail.com>
2024-12-26 18:15:01 +0530

    taildwind

--- React/taildwindpro/src/App.jsx
+++ React/taildwindpro/src/App.jsx
@@ -9,7 +9,17 @@ function App() {
       <h2 className='line-through uppercase m-36 text-5xl decoration-red-800'>asdkjhasikd asds as</h2>
 
       {/* [] */}
-      <div className='w-1/4 h-72 hover:bg-red-500 hover:text-[rgb(178,233,89)]  bg-[rgba(0,0,0,0.5)] text-[50px] text-[rgb(255,255,255)]'> Hello this</div>
+      <div className='w-1/4 h-72  hover:bg-red-500 hover:text-[rgb(178,233,89)]  bg-[rgba(0,0,0,0.5)] text-[50px] text-[rgb(255,255,255)]'> Hello this</div>
+
+      <div className="main flex justify-between">
+        <div className="main1 w-60 h-60 bg-red-500 border border-black">1</div>
+        <div className="main1 w-60 h-60 bg-red-500 border border-black">2</div>
+        <div className="main1 w-60 h-60 bg-red-500 border border-black">3</div>
+      </div>
+
+      <div className="main2 container bg-blue-500 mx-auto w-5/6">sdafas</div>
+      
+   
     </>
   )
 }
